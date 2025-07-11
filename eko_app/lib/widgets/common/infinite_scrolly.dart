6d522b0816571985c32df040eb11f6893638f99d
Eliah Reeves <ereeclimb@gmail.com>
2025-07-11 10:58:31 -0700

    popular + refactored scrolly a little

--- eko_app/lib/widgets/common/infinite_scrolly.dart
+++ eko_app/lib/widgets/common/infinite_scrolly.dart
@@ -33,8 +33,7 @@ class InfiniteScrolly<K, V> extends StatefulWidget {
   /// Function that receives the current list of items and returns the next chunk. The getter
   ///		must treat the list it recives as read only. The data may get out of date, but that is ok.
   ///   Also must return bool to signifiy if this is the last chunk
-  final Future<(List<MapEntry<K, V>>, bool)> Function(List<MapEntry<K, V>>)
-      getter;
+  final Future<(List<(K, V)>, bool)> Function(List<(K, V)>) getter;
 
   /// Builder function that takes a key and returns a widget to display it.
   final Widget Function(K) widget;
@@ -78,7 +77,7 @@ class InfiniteScrolly<K, V> extends StatefulWidget {
 }
 
 class _InfiniteScrollyState<K, V> extends State<InfiniteScrolly<K, V>> {
-  List<MapEntry<K, V>> objectPairs = [];
+  List<(K, V)> objectPairs = [];
   bool isEnd = false;
   bool isLoading = false;
 
@@ -113,7 +112,7 @@ class _InfiniteScrollyState<K, V> extends State<InfiniteScrolly<K, V>> {
       onRefresh: onRefresh,
       getter: onScroll,
       widget: widget.widget,
-      list: objectPairs.map((item) => item.key).toList(),
+      list: objectPairs.map((item) => item.$1).toList(),
       isEnd: isEnd,
       controller: widget.controller,
       loadingWidget: widget.loadingWidget,
