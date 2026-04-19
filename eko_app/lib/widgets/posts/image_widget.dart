3b366f838e17728f706b023e42bdeb0b696f1f46
Eric Chuang <ericchuang94@gmail.com>
2026-04-18 18:15:11 -0700

    full screen image

--- eko_app/lib/widgets/posts/image_widget.dart
+++ eko_app/lib/widgets/posts/image_widget.dart
@@ -3,18 +3,51 @@ import 'package:image_to_ascii/image_to_ascii.dart';
 
 class ImageWidget extends StatelessWidget {
   final AsciiImage ascii;
+  final VoidCallback? onTap;
 
-  const ImageWidget({super.key, required this.ascii});
+  const ImageWidget({super.key, required this.ascii, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return AsciiImageWidget(
-      ascii: ascii,
-      textStyle: TextStyle(
-        fontFamily: 'MartianMono',
-        fontWeight: FontWeight.w700,
-        fontSize: 40,
-        height: 1,
+    return GestureDetector(
+      onTap: onTap ?? () => _showFullScreen(context),
+      child: AsciiImageWidget(
+        ascii: ascii,
+        textStyle: TextStyle(
+          fontFamily: 'MartianMono',
+          fontWeight: FontWeight.w700,
+          fontSize: 40,
+          height: 1,
+        ),
+      ),
+    );
+  }
+
+  void _showFullScreen(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierColor: Colors.black,
+      builder: (context) => Dialog(
+        backgroundColor: Colors.transparent,
+        insetPadding: EdgeInsets.zero,
+        child: GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: InteractiveViewer(
+            minScale: 0.5,
+            maxScale: 4.0,
+            child: Center(
+              child: AsciiImageWidget(
+                ascii: ascii,
+                textStyle: TextStyle(
+                  fontFamily: 'MartianMono',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  height: 1,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
