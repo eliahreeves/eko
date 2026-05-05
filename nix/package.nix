2404dd44968dabf07346530266cfacab0a70dd92
Eliah Reeves <ereeclimb@gmail.com>
2026-05-05 14:34:46 -0700

    fix linux buidler for webcrypto

--- nix/package.nix
+++ nix/package.nix
@@ -9,6 +9,8 @@
   copyDesktopItems,
   wrapGAppsHook3,
   pkg-config,
+  cmake,
+  jq,
   makeDesktopItem,
 }:
 flutter.buildFlutterApplication {
@@ -18,6 +20,7 @@ flutter.buildFlutterApplication {
   autoPubspecLock = ../eko_app/pubspec.lock;
   gitHashes = {};
   vendorHash = lib.fakeHash;
+  dontUseCmakeConfigure = true;
 
   buildInputs = [
     gtk3
@@ -31,7 +34,27 @@ flutter.buildFlutterApplication {
     copyDesktopItems
     wrapGAppsHook3
     pkg-config
+    cmake
+    jq
   ];
+
+  preBuild = ''
+    webcrypto_root_uri="$(jq -r '.packages[] | select(.name=="webcrypto") | .rootUri' .dart_tool/package_config.json)"
+    if [ -z "$webcrypto_root_uri" ] || [ "$webcrypto_root_uri" = "null" ]; then
+      echo "webcrypto package not found in package_config.json"
+      exit 1
+    fi
+
+    webcrypto_root="''${webcrypto_root_uri#file://}"
+    cmake -S "$webcrypto_root/src" -B .dart_tool/webcrypto
+    cmake --build .dart_tool/webcrypto --target webcrypto
+  '';
+
+  preFixup = ''
+    gappsWrapperArgs+=(
+      --prefix LD_LIBRARY_PATH : "$out/app/eko/lib"
+    )
+  '';
 
   desktopItems = [
     (makeDesktopItem {
